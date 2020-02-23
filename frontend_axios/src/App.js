28ea49576e677ae115974603417133b237b49f35
luanesouza <luanesouza411@gmail.com>
2020-02-22 23:02:10 -0500

    removes extra code for lecture

--- frontend_axios/src/App.js
+++ frontend_axios/src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import PersonContainer from './components/PersonContainer'
 import { getPeopleFetch,
-        deletePeopleFetch,
-        createPeopleFetch,
-        updatePeopleFetch,
+        getPeopleAxios,
         } from './utils'
-const axios = require('axios');
-// const url = 'https://historical-figures.herokuapp.com/'
-const url = 'http://localhost:3000'
+
 
 class App extends Component {
   state = {
@@ -17,27 +13,9 @@ class App extends Component {
   }
 
   componentDidMount(){
-    // let data = getPeopleFetch()
 
-    // let data = setInterval(() => {  console.log(getPeopleFetch())}, 3000);
-    // console.log(data);
-
-    axios(`${url}/people`)
-      .then(response => {
-        if(response.status === 200 || response){
-          this.setState({
-            people: response.data,
-            error: false
-          })
-        } else {
-          this.setState({
-            error: true
-
-          })
-        }
-      })
-    // deletePeopleFetch(10)
   }
+
   render(){
 
     return (
@@ -45,8 +23,8 @@ class App extends Component {
         { this.state.error
         ?
         <>
-        <p id='line'> Something went wrong </p>
-        <img id='meme' src='https://i.kym-cdn.com/photos/images/original/000/768/910/412.png' alt='meme'/>
+          <p id='line'> Something went wrong </p>
+          <img id='meme' src='https://i.kym-cdn.com/photos/images/original/000/768/910/412.png' alt='meme'/>
         </>
         :
         <PersonContainer people={this.state.people}/>
--- frontend_axios/src/utils.js
+++ frontend_axios/src/utils.js
@@ -2,65 +2,13 @@ const axios = require('axios');
 const url = 'http://localhost:3000/people'
 
 
+
+
 const getPeopleFetch = () => {
-  return fetch(`${url}`)
-    .then(response => response.json())
-    .then((res) =>  {return res })
-}
-
-const createPeopleFetch = (data) => {
-  fetch(`${url}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Success:', data);
-  })
-}
-
-const deletePeopleFetch = (id) => {
-  fetch(`${url}/people/${id}`, {
-  method: 'DELETE',
-  })
-  .then(res => res.json())
-  .then(res => console.log('worked', res))
-}
-
-
-const updatePeopleFetch = (id) => {
-  let data = {name: 'Turing'}
-  fetch(`${url}/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Success:', data);
-  })
 }
 
 const getPeopleAxios = () => {
-  const response = axios.get(`${url}`)
-  .then(response => {
-    return response
-  })
 }
 
 
-
-// let data = {
-//   id: 10,
-//   name: 'Tatyana Fazlalizadeh',
-//   bio: 'is an American artist, activist, and freelance illustrator. She is best known as the creator of the campaign and art exhibition Stop Telling Women to Smile',
-//   image_url: 'https://images.squarespace-cdn.com/content/v1/566ca3e51115e0874922e571/1527697769922-9IYXS5CLRRRSNFQI2LPD/ke17ZwdGBToddI8pDm48kBXfl9523PPCMjiJNCtOhKtZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpx099H1iDMs77VbX2wfrKeigECPMbG3kEu0b-ipHJs1wyNrL1545Wed7woU4xgzO5Q/static1.squarespace.jpg',
-//   dob: 'October 12, 1985'
-// }
-
-export { getPeopleFetch, deletePeopleFetch, createPeopleFetch, updatePeopleFetch }
+export { getPeopleFetch, getPeopleAxios }
